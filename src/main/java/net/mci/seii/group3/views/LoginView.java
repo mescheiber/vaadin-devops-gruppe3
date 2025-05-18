2538a9e2dd9bd8ed18b94a34ce18d5d7454bb18c
U-OETZTAL\User(2641) <OETZTAL+User(2641)@c-03-914.oetztal.local>
2025-05-18 09:41:00 +0200

    Styling LoginView

--- src/main/java/net/mci/seii/group3/views/LoginView.java
+++ src/main/java/net/mci/seii/group3/views/LoginView.java
@@ -1,7 +1,11 @@
 package net.mci.seii.group3.views;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -11,20 +15,34 @@ import net.mci.seii.group3.model.User;
 import net.mci.seii.group3.service.AuthService;
 
 @Route(value = "", layout = MainLayout.class)
+@CssImport("./styles/shared-styles.css")
 public class LoginView extends VerticalLayout {
 
     public LoginView() {
+        setSizeFull();
+        setAlignItems(FlexComponent.Alignment.CENTER);
+        setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+
+        // Logo
+        Image logo = new Image("images/MCI_Logo.png", "MCI Logo");
+        logo.addClassName("logo");
+
+        // Titel
+        H1 title = new H1("AnwesenheitsApp");
+        title.addClassName("title");
+
+        // Felder
         TextField username = new TextField("Username");
         PasswordField password = new PasswordField("Password");
         Label status = new Label();
 
+        // Button
         Button login = new Button("Login", e -> {
             User user = AuthService.getInstance().login(username.getValue(), password.getValue());
 
             if (user != null) {
                 VaadinSession.getCurrent().setAttribute(User.class, user);
-                AuthService.getInstance().setAngemeldeterBenutzer(user); // richtig
-
+                AuthService.getInstance().setAngemeldeterBenutzer(user);
 
                 switch (user.getRole()) {
                     case ADMIN -> getUI().ifPresent(ui -> ui.navigate("admin"));
@@ -36,6 +54,13 @@ public class LoginView extends VerticalLayout {
             }
         });
 
-        add(username, password, login, status);
+        login.addClassName("button");
+
+        // Login-Form
+        VerticalLayout loginForm = new VerticalLayout(logo, title, username, password, login, status);
+        loginForm.setAlignItems(FlexComponent.Alignment.CENTER);
+        loginForm.addClassName("login-form");
+
+        add(loginForm);
     }
 }
